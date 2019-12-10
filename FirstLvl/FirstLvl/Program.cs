202eb7ec9df4272680c07cf9c0893401e80e601f
agrianess <56798293+agrianess@users.noreply.github.com>
2019-12-10 12:11:43 +0300

    init

---
+++ FirstLvl/FirstLvl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLvl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
