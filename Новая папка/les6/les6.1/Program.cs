06bb3023c102f5c289a1643dcb82b28669db699a
agrianess <56798293+agrianess@users.noreply.github.com>
2019-12-17 18:51:53 +0300

    les6

---
+++ Новая папка/les6/les6.1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les6._1
+{
+  //  Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double).
+  // Продемонстрировать работу на функции с функцией a* x^2 и функцией a* sin(x).
+  // Лукашенко Валентина
+
+ 
+        // Описываем делегат. В делегате описывается сигнатура методов, на
+        // которые он сможет ссылаться в дальнейшем (хранить в себе)
+        public delegate double Fun(double x, double a);
+
+        class Program
+        {
+            // Создаем метод, который принимает делегат
+            // На практике этот метод сможет принимать любой метод
+            // с такой же сигнатурой, как у делегата
+            public static void Table(Fun F, double x, double b, double a)
+            {
+           
+
+                Console.WriteLine("----- X ----- Y -----");
+                while (x <= b)
+                {
+                    Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
+                    x += 1;
+                }
+                Console.WriteLine("---------------------");
+            }
+        /// <summary>
+        /// a* x^2
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static double MyFuncAX2(double x, double a)
+            {
+                return a*x * x;
+            }
+
+        /// <summary>
+        /// a* sin(x)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static double MyFuncASin(double x, double a)
+        {
+            return a * Math.Sin(x);
+        }
+
+        static void Main()
+            {
+            
+                Console.WriteLine("Введите коэффициент a: ");
+                double a = double.Parse(Console.ReadLine());
+
+            // Создаем новый делегат и передаем ссылку на него в метод Table
+            Console.WriteLine("Таблица функции a* x^2:");
+            Table(MyFuncAX2, -2, 2,a);
+            Console.WriteLine("Таблица функции a* sin(x)");
+            Table(MyFuncASin, -2, 2, a);
+
+            Console.ReadLine();
+            }
+        
+
+    }
+}
---
+++ Новая папка/les6/les6.2/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace les6._2
+//Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+//а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум.Использовать массив(или список) делегатов, в котором
+//хранятся различные функции.
+//б) * Переделать функцию Load, чтобы она возвращала массив считанных значений.Пусть она возвращает минимум через параметр(с использованием модификатора out). 
+/// Лукашенко Валентина
+
+{
+    public delegate double Fun(double x);
+
+    class Program
+    {
+        #region Func
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double F2(double x)
+        {
+            return -x * x * x + 2;
+        }
+
+        public static double F3(double x)
+        {
+            return Math.Log(x)+2*x;
+        }
+
+        public static double F4(double x)
+        {
+            return Math.Tan(x);
+        }
+
+        public static double F5(double x)
+        {
+            return Math.Abs(x - 3) + 5;
+        }
+
+#endregion
+        public static void SaveFunc(Fun F, string fileName, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
+        public static double[] Load(string fileName, out double min)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            min = double.MaxValue;
+            double d;
+
+            List<double> listD = new List<double>();
+
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                listD.Add(d);
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+
+            double[] masZnach = listD.ToArray();
+
+            return masZnach;
+        }
+        static void Main(string[] args)
+        {
+          
+
+            List<string> listFun = new List<string>();
+            listFun.Add("x^2-50x+10");
+            listFun.Add("-x^3+2");
+            listFun.Add("Ln(X)+2*x^2");
+            listFun.Add("Tg(x^2)");
+            listFun.Add("|x-3|+5");
+
+            Console.WriteLine("Выберите необходимую функцию:");
+            Console.WriteLine();
+
+            foreach (var l in listFun)
+            {
+                Console.WriteLine($"Введите {listFun.IndexOf(l)} для выбора функции: {l}");
+                Console.WriteLine();
+            }
+
+            int typeFunc = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите начало отрезка значений х:");
+            double xStart = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите окончание отрезка значений х:");
+            double xFinish = double.Parse(Console.ReadLine());
+
+            switch(typeFunc)
+            {
+                case 0:
+                    SaveFunc(F1,"data.bin", xStart, xFinish, 0.5);
+                    break;
+                case 1:
+                    SaveFunc(F2, "data.bin", xStart, xFinish, 0.5);
+                    break;
+                case 2:
+                    SaveFunc(F3, "data.bin", xStart, xFinish, 0.5);
+                    break;
+                case 3:
+                    SaveFunc(F4, "data.bin", xStart, xFinish, 0.5);
+                    break;
+                case 4:
+                    SaveFunc(F5, "data.bin", xStart, xFinish, 0.5);
+                    break;
+
+            }
+
+
+            Console.WriteLine($"Минимальное значение функции  {listFun.ElementAt(typeFunc)} на отрезке от {xStart} до {xFinish} равно:");
+            Console.WriteLine(Load("data.bin"));
+            Console.ReadKey();
+        }
+    }
+}
+
